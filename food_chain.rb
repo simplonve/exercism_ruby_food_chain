185eeec122f0b3fa1614969e51544c48b2c4c014
jum-s <vincent.jumeaux@gmail.com>
2015-11-23 11:12:22 +0100

    add objects

--- food_chain.rb
+++ food_chain.rb
@@ -1,71 +1,41 @@
-class FoodChain
-	def self.verset(animal, petite_farce)
-		verset = "I know an old lady who swallowed a #{animal}.
-#{petite_farce}\n"
+class Verset
+	attr_accessor :animal, :petite_farce
+	def initialize(animal, petite_farce)
+		@animal = animal
+		@petite_farce = petite_farce
+	end
+
+	def build
+		verset = ""
 		animals.each do |a, s|
-			verset += s if animal != a
+		    verset = s + "\n"+ verset if animal != a
 			break if animal == a
 		end
-		return verset + animals[animal]
+		
+        "I know an old lady who swallowed a #{animal}.\n#{petite_farce}#{animals[animal]}\n#{verset}"
 	end
 
-	def self.animals
-		animals = {:fly => "I don't know why she swallowed the fly. Perhaps she'll die.", 
-			:spider => "She swallowed the spider to catch the fly.", 
-			:bird => "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.", 
-			:cat =>"She swallowed the cat to catch the bird.", 
-			:dog =>"", 
-			:goat =>"", 
-			:cow => "", 
-			:horse =>""}
+	def animals
+		{:fly => "I don't know why she swallowed the fly. Perhaps she'll die.", 
+	    :spider => "She swallowed the spider to catch the fly.", 
+		:bird => "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.", 
+		:cat =>"She swallowed the cat to catch the bird.", 
+		:dog =>"She swallowed the dog to catch the cat.", 
+		:goat =>"She swallowed the goat to catch the dog.", 
+		:cow => "She swallowed the cow to catch the goat.", 
+		}
 	end
+end
 
+class FoodChain
 	def self.song
-
-		song = "#{verset(:fly, "")}
-
-#{verset(:spider, "It wriggled and jiggled and tickled inside her.")}
-
-I know an old lady who swallowed a bird.
-How absurd to swallow a bird!
-#{animals[:bird]}
-#{animals[:spider]}
-#{animals[:fly]}
-
-I know an old lady who swallowed a cat.
-Imagine that, to swallow a cat!
-#{animals[:cat]}
-#{animals[:bird]}
-#{animals[:spider]}
-#{animals[:fly]}
-
-I know an old lady who swallowed a dog.
-What a hog, to swallow a dog!
-She swallowed the dog to catch the cat.
-#{animals[:cat]}
-#{animals[:bird]}
-#{animals[:spider]}
-#{animals[:fly]}
-
-I know an old lady who swallowed a goat.
-Just opened her throat and swallowed a goat!
-She swallowed the goat to catch the dog.
-She swallowed the dog to catch the cat.
-#{animals[:cat]}
-#{animals[:bird]}
-#{animals[:spider]}
-#{animals[:fly]}
-
-I know an old lady who swallowed a cow.
-I don't know how she swallowed a cow!
-She swallowed the cow to catch the goat.
-She swallowed the goat to catch the dog.
-She swallowed the dog to catch the cat.
-#{animals[:cat]}
-#{animals[:bird]}
-#{animals[:spider]}
-#{animals[:fly]}
-
+"#{Verset.new(:fly, "").build}
+#{Verset.new(:spider, "It wriggled and jiggled and tickled inside her.\n").build}
+#{Verset.new(:bird, "How absurd to swallow a bird!\n").build}
+#{Verset.new(:cat, "Imagine that, to swallow a cat!\n").build}
+#{Verset.new(:dog, "What a hog, to swallow a dog!\n").build}
+#{Verset.new(:goat, "Just opened her throat and swallowed a goat!\n").build}
+#{Verset.new(:cow, "I don't know how she swallowed a cow!\n").build}
 I know an old lady who swallowed a horse.
 She's dead, of course!
 "
